4a09ede1c286a1f50640011082faf8c437eae4a4
Evandro <evandrojramos@gmail.com>
2026-04-26 20:55:21 -0300

    coorigindo funções

--- miniprojeto.py
+++ miniprojeto.py
@@ -1,47 +1,5 @@
 #Mini Projeto do DIA 6
 
-#Função de validação pH
-def validar_ph(pH,pH_limite_minimo, pH_limite_maximo):
-    if pH > pH_limite_minimo:
-        return "Reprovado", f"O pH está acima do limite permitido{pH_limite_minimo}."
-    elif pH < pH_limite_maximo:
-        return "Reprovado", f"O pH está abaixo do limite permitido{pH_limite_maximo}."
-    else:
-        return "Aprovado", f"O pH está dentro do limite permitido."
-
-#Função de validação turbidez
-def validar_turbidez(turbidez,turbidez_limite_maximo):
-    if turbidez > turbidez_limite_maximo:
-        return "Reprovado", f"A turbidez está acima do limite permitido{turbidez_limite_maximo}."
-    else:
-        return "Aprovado", f"A turbidez está dentro do limite permitido."   
-
-#Função de validação coliformes
-def validar_coliformes(presenca):
-    if presenca == True:
-        return "Reprovado", f"A presença de coliformes totais indica contaminação."
-    else:
-        return "Aprovado", f"A presença de coliformes totais indica ausência de contaminação."    
-
-#Função de avaliação geral dos laudos
-def avaliar_laudos(numero,pH,turbidez,coliformes,validar_ph,validar_turbidez,validar_coliformes):
-    status_ph, motivo_ph = validar_ph(pH)
-    status_turbidez, motivo_turbidez = validar_turbidez(turbidez)
-    status_coliformes, motivo_coliformes = validar_coliformes(coliformes) 
-
-    #Retorna o status geral dos laudos
-    if status_ph == "Reprovado" and status_turbidez == "Reprovado" and status_coliformes == "Reprovado":
-        status = "Reprovado"
-    else:
-        status = "Aprovado"
-
-    print(f"\nLaudos: {numero}")
-    print(f"PH: {ph} {status_ph} {motivo_ph}")
-    print(f"Turbidez: {turbidez} {status_turbidez} {motivo_turbidez}")
-    print(f"Coliformes: {coliformes} {status_coliformes} {motivo_coliformes}")
-
-    return status_geral
-
 laudos =[
     {"numero": "LAB-2025-00325", "pH": 6.5, "Turbidez":75, "Coliformes": True},
     {"numero": "LAB-2025-00326", "pH": 7.5, "Turbidez":80, "Coliformes": False},
@@ -54,8 +12,50 @@ laudos =[
 #limites
 pH_limite_minimo = 6.5
 pH_limite_maximo = 8.5
-
 turbidez_limite_maximo = 100
+
+#Função de validação pH
+def validar_ph(pH,pH_limite_minimo, pH_limite_maximo):
+    if pH < pH_limite_minimo:
+        return "Reprovado", f"O pH está abaixo do limite permitido {pH_limite_minimo}."
+    elif pH > pH_limite_maximo:
+        return "Reprovado", f"O pH está acima do limite permitido {pH_limite_maximo}."
+    else:
+        return "Aprovado", f"O pH está dentro do limite permitido."
+ 
+#Função de validação turbidez
+def validar_turbidez(turbidez,turbidez_limite_maximo):
+    if turbidez > turbidez_limite_maximo:
+        return "Reprovado", f"A turbidez está acima do limite permitido{turbidez_limite_maximo}."
+    else:
+        return "Aprovado", f"A turbidez está dentro do limite permitido."   
+
+#Função de validação coliformes 
+def validar_coliformes(presenca):
+    if presenca == True:
+        return "Reprovado", f"A presença de coliformes totais indica contaminação."
+    else:
+        return "Aprovado", f"A presença de coliformes totais indica ausência de contaminação."    
+
+#Função de avaliação geral dos laudos
+def avaliar_laudos(numero, pH, turbidez, coliformes):
+    status_ph, motivo_ph = validar_ph(pH, pH_limite_minimo,pH_limite_maximo)
+    status_turbidez, motivo_turbidez = validar_turbidez(turbidez,turbidez_limite_maximo)
+    status_coliformes, motivo_coliformes = validar_coliformes(coliformes) 
+
+    #Retorna o status geral dos laudos
+    if status_ph == "Reprovado" or status_turbidez == "Reprovado" or status_coliformes == "Reprovado":
+        status_geral = "Reprovado"
+    else:
+        status_geral = "Aprovado"
+
+    print(f"\nLaudos: {numero}")
+    print(f"PH: {pH} {status_ph} {motivo_ph}")
+    print(f"Turbidez: {turbidez} {status_turbidez} {motivo_turbidez}")
+    print(f"Coliformes: {coliformes} {status_coliformes} {motivo_coliformes}")
+
+    return status_geral
+
 
 aprovados = 0
 reprovados = 0
